a4ce81b94ce1825d392e1409d04d728acea89042
Felipe Fahrion <felipefahrion@Felipes-MacBook-Pro.local>
2020-08-28 10:53:51 -0300

    add middleware on routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,36 @@
 const express = require('express')
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
 app.use(express.json())
 
 const projects = []
+
+function logRequests(req, res, next) {
+    const { method, url} = req
+
+    const logLabel = `[${method.toUpperCase()}] ${url}`
+
+    console.log(logLabel)
+    console.time(logLabel)
+
+    next()
+
+    console.timeEnd(logLabel)
+}
+
+function validateProjectId(req, res, next){
+    const {id} = req.params
+
+    if(!isUuid(id)){
+        return res.status(400).json({error: 'Invalid ID'})
+    }
+
+    return next()
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (req, res) => {
     const { title } = req.query
@@ -24,7 +50,7 @@ app.post('/projects', (req, res) => {
     return res.json(project)
 })
 
-app.put('/projects/:id', (req, res) => {
+app.put('/projects/:id',  (req, res) => {
     const { id } = req.params
     const { title, owner } = req.body    
 
